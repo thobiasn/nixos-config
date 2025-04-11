636d03c885969ed1ec1ef3b422d1f60b333c26ab
thobiasn <thobias.nordgaard@gmail.com>
2025-04-11 20:55:10 +0200

    like this?

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -23,7 +23,7 @@
 
       input = {
         kb_layout = "us,dk";
-        kb_options = "grp:switch";
+        kb_options = "grp:alt_shift_toggle";
         numlock_by_default = true;
         repeat_delay = 300;
         follow_mouse = 0;
