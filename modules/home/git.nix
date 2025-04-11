98faf90ca32a074905616946b955ee818b743c22
Frost-Phoenix <67cyril6767@gmail.com>
2025-04-11 16:41:29 +0200

    configure git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,8 +3,8 @@
   programs.git = {
     enable = true;
 
-    userName = "Frost-Phoenix";
-    userEmail = "67cyril6767@gmail.com";
+    userName = "thobiasn";
+    userEmail = "thobias.nordgaard@gmail.com";
 
     extraConfig = {
       init.defaultBranch = "main";
