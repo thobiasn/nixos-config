e202cc7131dea428b8d3f4069f4e979aa0a92ec2
thobiasn <thobias.nordgaard@gmail.com>
2025-04-12 12:06:19 +0200

    remove rkhunter

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -11,7 +11,6 @@
   
   environment.systemPackages = with pkgs; [
     aide          # Advanced Intrusion Detection Environment
-    rkhunter      # Rootkit hunter (optional)
   ];
   
   boot.kernel.sysctl = {
