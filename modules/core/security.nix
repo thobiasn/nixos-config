f2da6529daed9ebfe7455975e127bfe5d252c224
thobiasn <thobias.nordgaard@gmail.com>
2025-04-12 12:02:46 +0200

    hardening

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,7 +1,30 @@
-{ ... }:
+{ pkgs, ... }:
 {
   security.rtkit.enable = true;
   security.sudo.enable = true;
   security.pam.services.swaylock = { };
   security.pam.services.hyprlock = { };
+
+  ## 🧱 AppArmor or SELinux (optional but good if you want MAC framework)
+  # NOTE: Requires kernel and extra work, optional on desktop/laptop
+  # iisecurity.apparmor.enable = true;
+  
+  environment.systemPackages = with pkgs; [
+    aide          # Advanced Intrusion Detection Environment
+    pam_passwdqc  # Password quality checking
+    rkhunter      # Rootkit hunter (optional)
+  ];
+  
+  boot.kernel.sysctl = {
+    "kernel.kptr_restrict" = 2;
+    "kernel.dmesg_restrict" = 1;
+    "fs.protected_fifos" = 2;
+    "fs.protected_regular" = 2;
+    "fs.suid_dumpable" = 0;
+    "kernel.unprivileged_bpf_disabled" = 1;
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.send_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+  };
 }
