22898c6cef6949356c910ebb58b08532650c172e
thobiasn <thobias.nordgaard@gmail.com>
2025-04-14 09:29:03 +0200

    another external display

--- modules/home/hyprland/scripts/hypr-display-toggle.sh
+++ modules/home/hyprland/scripts/hypr-display-toggle.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 INTERNAL="eDP-1"
-EXTERNAL="HDMI-A-1"
+EXTERNAL_1="HDMI-A-1"
+EXTERNAL_2="DP-3"
 
-if hyprctl monitors | grep -q "$EXTERNAL"; then
+if hyprctl monitors | grep -q "$EXTERNAL_1" || hyprctl monitors | grep -q "$EXTERNAL_2"; then
     hyprctl keyword monitor "$INTERNAL, disable"
 else
     hyprctl keyword monitor "$INTERNAL, preferred, auto, 1"
